d67dae22099d76449e58d839a05e35525afae734
gita cliff <gitacliff48@gmail.com>
2019-05-10 12:11:23 +0300

    my new commitsss

--- api/src/main/java/org/openmrs/module/patientcolors/api/db/HibernatepatientcolorsDao.java
+++ api/src/main/java/org/openmrs/module/patientcolors/api/db/HibernatepatientcolorsDao.java
@@ -11,7 +11,6 @@ import org.springframework.stereotype.Repository;
 @Repository
 public class HibernatepatientcolorsDao implements PatientcolorsDao {
 	
-
 	private DbSessionFactory sessionFactory;
 	
 	/**
